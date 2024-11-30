6f4b1a0305066799905d46f29f5df6dadd356e36
Amantchowdhary31 <amantch2@gmail.com>
2024-11-30 23:51:35 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CertificateForm from "./CertificateForm";
+
+const App = () => {
+  return (
+    <div>
+      <CertificateForm></CertificateForm>
+    </div>
+  );
+};
+
+export default App;
